7504fdd94b483214d288ed43a4d0840a37d6cdb8
Tim Herreijgers <timtim3001@gmail.com>
2024-05-16 13:46:36 +0200

    Added widget for menu bar

---
+++ widgets/src/menubar.cpp
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) $originalComment.match("Copyright © (\d+)", 1, "-", "$today.year")$today.year. Tim Herreijgers
+ * Licensed using the MIT license
+ */
+ // You may need to build the project (run Qt uic code generator) to get "ui_MenuBar.h" resolved
+
+#include "widgets/menubar.h"
+#include "ui_MenuBar.h"
+
+namespace Widgets {
+MenuBar::MenuBar(QWidget *parent) :
+    QMenuBar(parent), ui(new Ui::MenuBar) {
+    ui->setupUi(this);
+}
+
+MenuBar::~MenuBar() {
+    delete ui;
+}
+} // Widgets
