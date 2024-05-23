1ee1c15633c3aabde41f964a583a013a79235c60
Tim Herreijgers <timtim3001@gmail.com>
2024-05-24 00:06:41 +0200

    Added observer pattern for LogEntries

--- presenters/src/log_presenter.cpp
+++ presenters/src/log_presenter.cpp
@@ -5,13 +5,17 @@
 
 #include "presenters/log_presenter.h"
 
+#include <iostream>
+
 namespace Presenters
 {
 
 LogPresenter::LogPresenter(Widgets::LogWidget & view, Model::LogDataContext& model)
         : m_view(view), m_model(model)
 {
-    view.setLogMessages(model.getLogEntries());
+    model.subscribeToLogEntiesChanged([&](const std::vector<Types::LogEntry>& logEntries) {
+        view.setLogMessages(logEntries);
+    });
 }
 
 } // namespace Presenters
