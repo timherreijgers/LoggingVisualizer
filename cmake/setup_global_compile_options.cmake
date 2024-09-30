2114e58280def6804afbcc5872b36d99ee5eda22
Tim Herreijgers <timtim3001@gmail.com>
2024-09-30 13:11:52 +0200

    Added build steps for more platforms

--- cmake/setup_global_compile_options.cmake
+++ cmake/setup_global_compile_options.cmake
@@ -1,6 +1,6 @@
 function(setup_global_compile_options)
     if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-        add_compile_options(/W3 /WX /wd4996)
+        add_compile_options(/W3 /WX /wd4996 /MP)
     else ()
         add_compile_options(-Werror -Wextra -Wall -Wpedantic)
     endif ()
