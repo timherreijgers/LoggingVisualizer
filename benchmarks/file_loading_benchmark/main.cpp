26ff42167a78c26e3d676e9681581162f847db87
Tim Herreijgers <timtim3001@gmail.com>
2024-10-09 01:00:25 +0200

    Updated benchmark

--- benchmarks/file_loading_benchmark/main.cpp
+++ benchmarks/file_loading_benchmark/main.cpp
@@ -6,9 +6,11 @@
 #include "log_data_context.h"
 
 #include "benchmark/benchmark.h"
+#include "model/log_data_context_factory.h"
 
 #include <array>
 #include <cstddef>
+#include <fstream>
 
 static constexpr std::array m_defaultLines =
     {
@@ -18,6 +20,8 @@ static constexpr std::array m_defaultLines =
         "[00-00-2000 00.00.03][WARNING] This is a warning message",
         "[00-00-2000 00.00.04][ERROR] This is a error message",
         "[00-00-2000 00.00.05][CRITICAL] This is a critical message"};
+
+static constexpr std::string_view fileName = "dummy_log.log";
 
 class MockFile : public Model::IFileReader
 {
@@ -58,6 +62,19 @@ private:
     }
 };
 
+static void writeDummyLogFile()
+{
+    std::ofstream dummyLogFile{fileName};
+
+    for (auto i = 0; i < 10'000; i++)
+    {
+        for (const auto line : m_defaultLines)
+        {
+            dummyLogFile << line << "\n";
+        }
+    }
+}
+
 static void BM_FileReadingSmallFile(benchmark::State& state)
 {
     Model::LogDataContext context(std::make_unique<MockFile>(100));
@@ -71,7 +88,7 @@ static void BM_FileReadingSmallFile(benchmark::State& state)
 
 static void BM_FileReadingBigFile(benchmark::State& state)
 {
-    Model::LogDataContext context(std::make_unique<MockFile>(16'000'000));
+    Model::LogDataContext context(std::make_unique<MockFile>(1'000'000));
 
     for (auto _ : state)
     {
@@ -80,8 +97,23 @@ static void BM_FileReadingBigFile(benchmark::State& state)
     }
 }
 
+static void BM_FullyReadingActualFile(benchmark::State& state)
+{
+    writeDummyLogFile();
+    auto context = Model::LogDataContextFactory::createContext();
+
+    for (auto _ : state)
+    {
+        context->openFile(fileName);
+        context->closeFile();
+    }
+
+    std::filesystem::remove(fileName);
+}
+
 BENCHMARK_MAIN();
 
 // We need to set the Iterations to 1 since the code will only be run once when the file is loaded. It needs to be fast the first time
 BENCHMARK(BM_FileReadingSmallFile)->Unit(benchmark::kMicrosecond)->Iterations(1);
 BENCHMARK(BM_FileReadingBigFile)->Unit(benchmark::kMillisecond)->Iterations(1);
+BENCHMARK(BM_FullyReadingActualFile)->Unit(benchmark::kMillisecond)->Iterations(1);
