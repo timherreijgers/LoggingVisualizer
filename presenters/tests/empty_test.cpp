eb9b25de363919046be61bd811f82ccef417063d
Tim Herreijgers <timtim3001@gmail.com>
2024-11-16 11:42:39 +0100

    Added presenters test project

---
+++ presenters/tests/empty_test.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright © 2024 Tim Herreijgers
+ * Licensed using the MIT license
+ */
+
+#include "gtest/gtest.h"
+
+namespace Presenters::Tests 
+{
+
+TEST(EmptyTest, AssertTrue)
+{
+    ASSERT_TRUE(true);
+}
+
+} // namespace Presenters::Tests
