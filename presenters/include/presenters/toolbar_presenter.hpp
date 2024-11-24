080548cbf4ca5f048afbc442c9b046047d9f696d
Tim Herreijgers <timtim3001@gmail.com>
2024-11-24 19:50:06 +0100

    Added interface for Toolbar

--- presenters/include/presenters/toolbar_presenter.hpp
+++ presenters/include/presenters/toolbar_presenter.hpp
@@ -7,7 +7,7 @@
 
 #include "model/ilog_data_context.hpp"
 #include "presenters/base_presenter.hpp"
-#include "widgets/toolbar.hpp"
+#include "widgets/itoolbar.hpp"
 
 namespace Presenters
 {
@@ -15,10 +15,10 @@ namespace Presenters
 class ToolbarPresenter : public BasePresenter
 {
 public:
-    explicit ToolbarPresenter(Windows::WindowManager& manager, Widgets::Toolbar& view, Model::ILogDataContext& model);
+    explicit ToolbarPresenter(Windows::WindowManager& manager, Widgets::IToolbar& view, Model::ILogDataContext& model);
 
 private:
-    Widgets::Toolbar& m_view;
+    Widgets::IToolbar& m_view;
     Model::ILogDataContext& m_model;
 
     void filterTextChanged(const std::string& text);
