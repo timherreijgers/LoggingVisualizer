4c5bdce03f87107716e5ac332bb46ae0fa19f82e
Tim Herreijgers <timtim3001@gmail.com>
2025-07-30 00:17:02 +0200

    Added conan support

---
+++ conanfile.py
@@ -0,0 +1,20 @@
+#  Copyright © 2025 Tim Herreijgers
+#  Licensed using the MIT license
+
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout
+
+class MyProjectConan(ConanFile):
+    name = "LoggingVisualizer"
+    version = "0.1"
+    settings = "os", "compiler", "build_type", "arch"
+    requires = [
+        "benchmark/1.9.4",
+        "gtest/1.16.0",
+        "yaml-cpp/0.8.0",
+        "qt/6.5.3"
+    ]
+    generators = "CMakeDeps"
+
+    def layout(self):
+        cmake_layout(self)
